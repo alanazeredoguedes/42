9144ec21311b380e9c758518c260ed14d1b8b3be
AlanGuedes <alanguedes00@live.com>
2023-09-06 22:28:14 -0300

    Update

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,13 +1,34 @@
+#include <stdio.h>
+
 void	ft_sort_int_tab(int *tab, int size)
 {
-	
+	int	aux;
+	int	i;
 
+	i = 0;
+	while (i < size)
+	{
+		if( tab [i] < tab [size - 1 - i])
+		{
+			aux = tab [size - 1 - i];
+			tab [size - 1 - i] = tab [i];
+			tab [i] = aux;
+		}
+		i++;
+	}
 }
 
 int	main(void)
 {
-	int array_int[10] = { 4, 3, 2, 1, 5, 9, 6, 7, 8, 10}
+	int 	array_int[10] = { 5, 9, 8, 7, 6, 10, 4, 3, 2, 1};
+	int 	i;
 
+	i = 0;
 	ft_sort_int_tab(array_int, 10);
+	while (i<10)
+	{
+		printf("%d", array_int[i]);
+		i++;
+	}
 	return (0);
 }
